f9f507aaa18ee7d55abaf74f8cbf71e9853c84bd
SirNyble <thomasrhys14@gmail.com>
2017-10-23 21:48:32 -0300

    Prettyfying the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import logo from './logo.png';
+import logo from "./logo.png";
 
 // The Header creates links that can be used to navigate
-// between routes.
-const Header = () => (
-    <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Prying Panda</h1>
-    </header>
-);
+// between routes. Also displays the Header bar
+class Header extends React.Component {
+    render() {
+        return <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Alliteration With Wolf</h1>
+        </header>
+    }
+}
 
 export default Header;
