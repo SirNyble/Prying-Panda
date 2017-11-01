872ed3e9e37c24f2108ab00739d711250c7f1027
SirNyble <thomasrhys14@gmail.com>
2017-10-31 23:45:59 -0300

    Add a basic anonymous ask text area, with validation.

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,7 +1,7 @@
 import "./User.css";
 import axios from 'axios';
 import React from "react";
-import { Grid, Image, Col} from "react-bootstrap";
+import { Grid, Image, Row, Col, FormGroup, FormControl, HelpBlock} from "react-bootstrap";
 
 // The User component
 // Displays a users profile
@@ -10,6 +10,7 @@ class User extends React.Component {
         super(props);
 
         this.state = {
+            userQuestion: "",
             profileLoaded: false,
             userName: props.match.params.userName,
             bannerPicture: "",
@@ -17,6 +18,20 @@ class User extends React.Component {
             errors: ""
         };
         console.log(this.state);
+    }
+
+    getValidationState() {
+        console.log("VALIDATING");
+        const length = this.state.userQuestion.length;
+        if (length > 10) return 'success';
+        else if (length > 5) return 'warning';
+        else if (length > 0) return 'error';
+        return null;
+      }
+
+      handleChange(e) {
+        console.log(e.target); 
+        this.setState({ userQuestion: e.target.value });
     }
 
     componentDidMount() {
@@ -34,7 +49,9 @@ class User extends React.Component {
                 errors: err.response.data.error
             });
         });
-      }
+    }
+
+
 
     render() {
         if(this.state.errors !== "") {
@@ -49,11 +66,34 @@ class User extends React.Component {
             return <div><p>LOADING</p></div>
         }
         return (
-            <Grid  className="pictureArea">
-            <Col  className="user">
-                <Image src={this.state.profilePicture} className="profile" alt="logo" rounded />
-                <Image src={this.state.bannerPicture} className="banner"/>
-            </Col>
+            <Grid>
+                <Row>
+                    <Col  className="user">
+                        <Image src={this.state.profilePicture} className="profile" alt="logo" rounded />
+                        <Image src={this.state.bannerPicture} className="banner"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={4}>
+                        <form>
+                            <FormGroup
+                            controlId="formBasicText"
+                            validationState={this.getValidationState()}
+                            >
+                                <FormControl
+                                    componentClass="textarea"
+                                    type="text"
+                                    value={this.state.userQuestion}
+                                    placeholder="What is your burning question?"
+                                    onChange={this.handleChange.bind(this)}
+                                />
+                                <FormControl.Feedback />
+                                <HelpBlock>Validation is currently based on string length.</HelpBlock>
+                            </FormGroup>
+                        </form>
+                    </Col>
+                </Row>
+                <p className="filler"> asdlfjksdfjl</p>
             </Grid>
             
         );
